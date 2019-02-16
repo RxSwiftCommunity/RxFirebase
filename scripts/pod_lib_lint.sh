b1b1e2462012dbef6f4bdd927349e36dfc85901b
Arnaud Dorgans <arnaud.dorgans@gmail.com>
2019-02-17 00:45:35 +0100

    travis script

--- scripts/pod_lib_lint.sh
+++ scripts/pod_lib_lint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 for pod in $(find . -name "*.podspec" -type f -exec basename {} ';' );do
 	push=0
+  travis=0
 	if [[ $1 == 'push' ]]; then
                 push=1
         fi
+  if [[ $1 == 'travis' ]]; then
+    travis=1
+  fi
  	if [[ $pod == 'RxFirebase.podspec' ]]; then
 			continue
         fi
@@ -12,6 +16,9 @@ for pod in $(find . -name "*.podspec" -type f -exec basename {} ';' );do
 	else
 		command="pod trunk push $pod --allow-warnings  --verbose"
 	fi
+  if [[ $travis == 1 ]]; then
+    command="travis_wait 30 $command"
+  fi
 	echo $command
 	$command ; result=$?
 	if [[ $result != 0 ]]; then
