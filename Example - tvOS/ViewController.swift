84205f00435547144f5a392b8c5da58ad1a8777e
Arnaud Dorgans <arnaud.dorgans@gmail.com>
2019-03-20 13:36:49 +0100

    add tvOS example app

---
+++ Example - tvOS/ViewController.swift
@@ -0,0 +1,18 @@
+// RxFirebase
+// Copyright © 2019 RxSwiftCommunity. All rights reserved.
+
+import UIKit
+import RxFirebaseStorage
+import RxFirebaseDatabase
+import RxFirebaseAuthentication
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
